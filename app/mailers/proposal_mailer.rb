0367e096578632cef50a19c7b8b5b1df2667b81a
Emmanuel ROSE <roseemmanuel@hotmail.com>
2020-11-16 12:49:22 +0100

    proposal mailer

--- app/mailers/proposal_mailer.rb
+++ app/mailers/proposal_mailer.rb
@@ -6,14 +6,14 @@ class ProposalMailer < ApplicationMailer
   #   en.gift_mailer.confirmation.subject
   #
   def information(selection)
-    @selection = selection
-    attachments["Récapitulatif de la sélection No. #{@selection.id} du #{@selection.date}.pdf"] = WickedPdf.new.pdf_from_string(
-      render_to_string(pdf: 'todo', template: 'todo.pdf.erb', layout: 'pdf.html'), { :hash_with_wickedpdf_options }
-    )
-    mail(
-      to:       @selection.fournisseur.email,
-      subject:  "Votre dépôt chez Graîne de mômes"
-    )
+    # @selection = selection
+    # attachments["Récapitulatif de la sélection No. #{@selection.id} du #{@selection.date}.pdf"] = WickedPdf.new.pdf_from_string(
+    #   render_to_string(pdf: 'todo', template: 'todo.pdf.erb', layout: 'pdf.html'), { :hash_with_wickedpdf_options }
+    # )
+    # mail(
+    #   to:       @selection.fournisseur.email,
+    #   subject:  "Votre dépôt chez Graîne de mômes"
+    # )
   end
 
   # def pdf_attachment_method(todo_id)
