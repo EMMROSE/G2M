3f06af54dbd447f0ba333ca25116ab20b2f8a5d3
Emmanuel ROSE <roseemmanuel@hotmail.com>
2020-11-17 12:22:51 +0100

    attahcment PDF

--- app/mailers/proposal_mailer.rb
+++ app/mailers/proposal_mailer.rb
@@ -8,7 +8,7 @@ class ProposalMailer < ApplicationMailer
   def information(selection, pdf)
     @selection = selection
     attachments["Sélection n° #{@selection.id} du #{@selection.date}.pdf"] = WickedPdf.new.pdf_from_string(
-      render_to_string(pdf: 'todo', template: 'selections/show.html.erb', layout: 'pdf.html', page_size: 'A4', background: false, no_background: true, lowquality: true, zoom: 1, dpi: 300, encoding:"UTF-8")
+      render_to_string(pdf: 'Sélection n° #{@selection.id} du #{@selection.date}.', template: 'selections/show.html.erb', layout: 'pdf.html', page_size: 'A4', background: false, no_background: true, lowquality: true, zoom: 1, dpi: 300, encoding:"UTF-8")
     )
     # mail(to: todo.owner.email, subject: 'Your todo PDF is attached', todo: todo)
     mail(
