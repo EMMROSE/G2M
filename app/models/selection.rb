47324069f8487b38207b979fe9cde43ef90594c8
Emmanuel ROSE <roseemmanuel@hotmail.com>
2020-11-19 10:46:45 +0100

    csv encoding

--- app/models/selection.rb
+++ app/models/selection.rb
@@ -12,7 +12,8 @@ class Selection < ApplicationRecord
   end
 
   def generate_csv
-    CSV.generate(encoding: 'UTF-8') do |csv|
+    csv_options = { col_sep: ',', encoding: 'ISO-8859-1'}
+    CSV.generate(csv_options) do |csv|
       csv << ["Handle","Title","Body (HTML)","Vendor","Type","Tags","Published","Option1 Name","Option1 Value","Option2 Name","Option2 Value","Option3 Name","Option3 Value","Variant SKU","Variant Grams","Variant Inventory Tracker","Variant Inventory Qty","Variant Inventory Policy","Variant Fulfillment Service","Variant Price","Variant Compare At Price","Variant Requires Shipping","Variant Taxable","Variant Barcode","Image Src","Image Position","Image Alt Text","Gift Card","SEO Title","SEO Description","Google Shopping / Google Product Category","Google Shopping / Gender","Google Shopping / Age Group","Google Shopping / MPN","Google Shopping / AdWords Grouping","Google Shopping / AdWords Labels","Google Shopping / Condition","Google Shopping / Custom Product","Google Shopping / Custom Label 0","Google Shopping / Custom Label 1","Google Shopping / Custom Label 2","Google Shopping / Custom Label 3","Google Shopping / Custom Label 4","Variant Image","Variant Weight Unit","Variant Tax Code","Cost per item"]
       self.products.each do |product|
         image_url = "http://res.cloudinary.com/grainedemomes/image/upload/" + product.photo.key.to_s
